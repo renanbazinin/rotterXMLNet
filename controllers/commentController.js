7bc78fcd2180a5352a1abdf1554160adee1aea65
renan <renanbazinin2@gmail.com>
2023-02-19 08:44:29 +0200

    dubugg notes

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -81,6 +81,7 @@ router.route("/rotter").get( async (req,res) =>{
     
     console.log("Before ");
     console.log(xmlString);
+    console.log("last connection to server: \n" + Date.now() );
     let finalXML = xmlString;
     
     return res.send(finalXML);
