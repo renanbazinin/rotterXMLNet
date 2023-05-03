3e6f7f1aa18810d5aa2081691505d8a45e57c0fe
renanbazinin <renanbazinin2@gmail.com>
2023-05-03 22:33:17 +0300

    Add files via upload

---
+++ functions/api.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const cors = require("cors");
+const router = express.Router();
+
+
+const commentController = require("../controllers/commentController");
+const Serverless = require("serverless-http");
+
+
+const app = express();
+//const port = 8001;
+const port = process.env.PORT;
+app.use(cors());
+
+//They converted  what
+app.use(express.json());
+app.use(express.urlencoded({extended:true}))
+
+app.use("/.netlify/functions/api",commentController)
+
+
+app.listen(port,()=>{
+    console.log("App is listening")
+    console.log("http://localhost:"+port)
+}
+
+)
+
+module.exports.handller =Serverless(app);
